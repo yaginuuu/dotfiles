9754dc6a45c4a67bf7eeeb2eaa1ee6fe8c9c9cbf
yaginuuu <yaginuuu56@gmail.com>
2014-08-30 03:31:31 +0900

    setup.sh追加

---
+++ setup.sh
@@ -0,0 +1,7 @@
+ln -sf ~/.dotfiles/.oh-my-zsh ~/.oh-my-zsh
+ln -sf ~/.dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/.dotfiles/.gitignore ~/.gitignore
+ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/.dotfiles/.vimrc ~/.vimrc
+ln -sf ~/.dotfiles/.vimrc.bundle ~/.vimrc.bundle
+ln -sf ~/.dotfiles/.zshrc ~/.zshrc
